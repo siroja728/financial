06c00a5b525be9a11bc722f5c5e5272a03074858
Serhii Yemelianov <yemelyanov1994@gmail.com>
2025-06-18 02:15:57 +0300

    Fix assetPrefix configuration in next.config.ts for production environment

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/financial" : "",
+  assetPrefix: isProd ? "/financial" : "",
   images: {
     unoptimized: true,
   },
