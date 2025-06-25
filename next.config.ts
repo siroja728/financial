87dbf1c465db1a7f010fa0af94af65f7b1e8c2a0
Serhii Yemelianov <yemelyanov1994@gmail.com>
2025-06-26 00:48:55 +0300

    refactor: remove GitHub Pages deployment workflow and simplify Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 // When will be deploy not to GH Pages but to Vercel or other hosting, we need modify config
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/financial" : "",
-  assetPrefix: isProd ? "/financial" : "",
-  images: {
-    unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: "/financial",
-  },
-};
+const nextConfig: NextConfig = {};
 
-export default isProd ? nextConfig : {};
+export default nextConfig;
