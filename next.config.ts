503c57a3cf98fc657aee348740d19fde7c5ac2ff
Serhii Yemelianov <yemelyanov1994@gmail.com>
2025-07-21 12:59:53 +0300

    feat: set runtime to nodejs in next.config.ts for server-side compatibility

--- next.config.ts
+++ next.config.ts
@@ -3,4 +3,6 @@ import type { NextConfig } from "next";
 // When will be deploy not to GH Pages but to Vercel or other hosting, we need modify config
 const nextConfig: NextConfig = {};
 
+export const runtime = "nodejs";
+
 export default nextConfig;
